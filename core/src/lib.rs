150f61cbdf087d8f8e710cfadacec0d1b55d77a2
bokuweb <bokuweb12@gmail.com>
2022-07-09 14:33:14 +0900

    encoderf

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -17,7 +17,14 @@ pub use webp::*;
 pub fn diff<P: AsRef<Path>>(input: &CompareInput<P>) -> Result<DiffOutput, ImageDiffError> {
     let decoded1 = decode(input.actual_filename.as_ref())?;
     let decoded2 = decode(input.expected_filename.as_ref())?;
+    let p = "";
     let result = compare(input)?;
-    encode("a", &result.diff_image, result.width, result.height).unwrap();
+    encode(
+        input.diff_filename.as_ref().unwrap(),
+        &result.diff_image,
+        result.width,
+        result.height,
+    )
+    .unwrap();
     Ok(result)
 }
