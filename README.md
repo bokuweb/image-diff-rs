2930920877cecd74011b2ce9357f06212412f80a
bokuweb <bokuweb@bokuwebnoair.lan>
2023-11-18 11:52:36 +0900

    fix

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # image-diff-rs
 
 ``` sh
-$ cd wasm && cargo rustc --target=wasm32-unknown-emscripten -- -C link-args="-s EXPORTED_FUNCTIONS=['_diff'] -s STANDALONE_WASM -s --no-entry"
+$ cd wasm && cargo rustc --target=wasm32-unknown-emscripten -- -C link-args="-s EXPORTED_FUNCTIONS=['_diff'] -s STANDALONE_WASM -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s --no-entry"
 ```
--- gxx_personality_v0_stub.cpp
+++
@@ -1,7 +0,0 @@
-// https://stackoverflow.com/questions/67474533/error-in-compiling-rustemcc%20-c%20gxx_personality_v0_stub.cpp-into-webassembly-using-emscripten-on-windows
-#include "unwind.h"
-#include "stdint.h"
-
-extern "C" _Unwind_Reason_Code __gxx_personality_v0(int version, _Unwind_Action actions, uint64_t exceptionClass, _Unwind_Exception* unwind_exception, _Unwind_Context* context) {
-    return  _URC_NO_REASON;
-}
