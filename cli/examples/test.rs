a65c79eae341a56ecf97abbc15ddac681ff2001a
bokuweb <bokuweb12@gmail.com>
2023-11-26 15:12:23 +0900

    chore: remove cli for now (#9)
    Co-authored-by: bokuweb <bokuweb@bokuwebnoair.lan>

--- cli/examples/test.rs
+++
@@ -1,9 +0,0 @@
-// use image_diff_cli::*;
-//
-pub fn main() {
-    //     diff_files(DiffFilesInput::new(
-    //         "./fixtures/sample0.webp",
-    //         "./fixtures/sample1.webp",
-    //         "./diff.webp",
-    //     ));
-}
